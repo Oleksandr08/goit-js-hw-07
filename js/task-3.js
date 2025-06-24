da00ffaed01cb8bb27f288218de3298bdba57140
Oleksandr08 <is4uk2011@icloud.com>
2025-06-24 09:41:07 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,7 @@
-function getElementWidth(content, padding, border) {
-    const contentWith = parseFloat(content);
-    const paddingWith = parseFloat(padding);
-    const borderWith = parseFloat(border);
-    return contentWith + 2 * paddingWith + 2 * borderWith 
-}
-    
+const myInput = document.querySelector('#name-input');
+const myOutput = document.querySelector('#name-output');
 
-    console.log(getElementWidth("50px", "8px", "4px")); // 74
-    console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-    console.log(getElementWidth("200px", "0px", "0px")); // 200
-
-
+myInput.addEventListener('input', () => {
+  const trimmedValue = myInput.value.trim();
+  myOutput.textContent = trimmedValue === '' ? 'Anonymous' : trimmedValue;
+});
